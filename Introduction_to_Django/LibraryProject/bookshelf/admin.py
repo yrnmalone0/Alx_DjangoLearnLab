30f7415fdba89b9ce106ed6ec0cdde77481e1c6c
yrnmalone0 <obedbadu04@gmail.com>
2025-11-07 03:56:14 +0000

    customized django admin interface by adding a list view and search functionality

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,7 +4,7 @@ from .models import Book
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
 
 
