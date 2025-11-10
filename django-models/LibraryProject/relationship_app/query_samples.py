60c521eaf33cdbb2f2e18244a9b032e6079ba558
yrnmalone0 <obedbadu04@gmail.com>
2025-11-10 04:06:32 +0000

    corrected few error in 'query_samples'

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,6 @@
-from .models import Book, Library, Librarian
+from .models import Book, Library, Librarian, Author
 # Query all books by a specific author.
-Book.objects.filter(authors_name = "John Doe")
+Author.objects.get(name = author_name).objects.filter(author=author)
 
 # List all books in a library.
 Library.objects.get(name=library_name).books.all()
