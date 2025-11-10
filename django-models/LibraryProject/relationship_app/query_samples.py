ee163a0bc8407da3ff75275c30bd79dacf5fd669
yrnmalone0 <obedbadu04@gmail.com>
2025-11-10 03:58:19 +0000

    corrected checks for “List all books in a library.”

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from .models import Book, Library, Librarian
 Book.objects.filter(authors_name = "John Doe")
 
 # List all books in a library.
-Library.objects.get(name="Central Library")
+Library.objects.get(name=Library_name), books.all()
 
 
 # Retrieve the librarian for a library.
