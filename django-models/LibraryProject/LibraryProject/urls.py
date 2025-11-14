5b9e04e17a3b1830d40ecff1cd4736fd03732baa
yrnmalone0 <obedbadu04@gmail.com>
2025-11-14 19:00:32 +0000

    worked with Django views and URL configuration

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from relationship_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', views.book_list, name='book-list'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . models import Book
+from LibraryProject import views
 
-# Create your views here.
+# function-based view that lists all books stored in db
+def book_list(request):
+    """Retrieves all books and renders a template displaying the list"""
+    books = Book.objects.all()  # Fetch all book instances from the database
+    context = {'book_list' : books}  #create a context dictionary with book list
+    return render(request, 'books/list_books.html', context)
