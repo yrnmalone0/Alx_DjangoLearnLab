284e1a1a1aef072634b0347c4a797a66188602e3
yrnmalone0 <obedbadu04@gmail.com>
2025-11-14 09:45:34 +0000

    corrected 'Query all books by a specific author' to the right code base

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from .models import Book, Library, Librarian, Author
 # Query all books by a specific author.
 Author.objects.get(name = author_name)
-Book.objects.filter(authors__name=author_name)
+Book.objects.filter(author=author)
 
 # List all books in a library.
 Library.objects.get(name=library_name).books.all()
