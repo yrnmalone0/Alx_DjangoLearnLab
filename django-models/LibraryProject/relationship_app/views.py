c520766156845b6716b2328be30011327c4b81a7
yrnmalone0 <obedbadu04@gmail.com>
2025-11-15 02:40:14 +0000

    imported Library model into 'relationship_app/views.py'

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from . models import Book, Library
+from . models import Book
+from .models import Library
 from LibraryProject import views
 
 # function-based view that lists all books stored in db
