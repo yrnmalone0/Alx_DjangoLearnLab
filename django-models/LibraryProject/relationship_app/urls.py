bffb5dd7e31f51bc1da310e0048c93c8c7fd9b2b
yrnmalone0 <obedbadu04@gmail.com>
2025-11-15 15:16:58 +0000

    correcting (relationship_app/urls.py) urlpatterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import list_books
-from .views import LibraryDetailView
+from .views import register
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.book_list, name='book-list'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
-    path('register/', views.SignUpView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration/login.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -33,7 +33,7 @@ class LibraryDetailView(DetailView):
 # Utilize Django’s built-in views and forms for handling user authentication. You will need to create views for user login, logout, and registration.
 
 # Registration view
-class SignUpView(CreateView):
+class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
