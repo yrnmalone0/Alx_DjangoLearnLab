8b6a5dfce6ce061d8b7e73d3416a9091a08e3839
yrnmalone0 <obedbadu04@gmail.com>
2025-11-15 15:44:23 +0000

    correcting (relationship_app/urls.py) urlpatterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('books/', views.book_list, name='book-list'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/login.html'), name='logout'),
 ]
