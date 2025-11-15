d768277426766ba64f6bf687d0832dc1588baa99
yrnmalone0 <obedbadu04@gmail.com>
2025-11-15 02:27:59 +0000

    added a class-based view to display details of a specific library

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from . models import Book
 from LibraryProject import views
 
@@ -8,3 +9,15 @@ def book_list(request):
     books = Book.objects.all()  # Fetch all book instances from the database
     context = {'book_list' : books}  #create a context dictionary with book list
     return render(request, 'relationship_app/list_books.html', context)
+
+# class-based view that displays details for a specific library, listing all books 
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/library_detail.html'
+
+    def get_context_data(self, **kwargs):
+        """Injects additional context data specific to the book."""
+        context = super().get_context_data(**kwargs)  # Get default context data
+        book = self.get_object()  # Retrieve the current book instance
+        context['authors'] = book.authors.all()  # Add authors related to the book
+        return context
