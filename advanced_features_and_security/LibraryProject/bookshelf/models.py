5766f8d7507aaee0034a57f8ed2f35bf29e27b27
yrnmalone0 <obedbadu04@gmail.com>
2025-11-23 02:25:43 +0000

    Created a User Manager for Custom User Model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -16,3 +17,32 @@ class Book(models.Model):
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+
+"""
+Implement a custom user manager that handles user creation and queries, ensuring it can manage the added fields effectively.
+
+Custom Manager Functions to Implement:
+create_user: Ensure it handles the new fields correctly.
+create_superuser: Ensure administrative users can still be created with the required fields.
+"""
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
--- advanced_features_and_security/LibraryProject/relationship_app/views.py
+++ advanced_features_and_security/LibraryProject/relationship_app/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.base_user import BaseUserManager
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from . models import Book
@@ -125,31 +124,5 @@ def delete_book(request, book_id):
     # Logic to delete a book
     pass
 
-"""
-Implement a custom user manager that handles user creation and queries, ensuring it can manage the added fields effectively.
 
-Custom Manager Functions to Implement:
-create_user: Ensure it handles the new fields correctly.
-create_superuser: Ensure administrative users can still be created with the required fields.
-"""
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(username, email, password, **extra_fields)
 
