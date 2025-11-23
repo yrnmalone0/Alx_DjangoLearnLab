604bd70501494df439e0aa9a194c76dfb0cf3574
yrnmalone0 <obedbadu04@gmail.com>
2025-11-23 02:31:50 +0000

    Integrated the Custom User Model into Admin(bookshelf/admin.py)

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Book
+from ..relationship_app.models import UserProfile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+#from .models import UserProfile
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
 
@@ -8,4 +13,33 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
 
 
+"""
+Modify the Django admin to support the custom user model, ensuring that administrators can 
+manage users effectively through the Django admin interface.
+Define a custom ModelAdmin class that includes configurations for the additional fields in your user model.
+"""
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'UserProfile'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+    fieldsets = UserAdmin.fieldsets + (
+        (_('User Profile'), {'fields': ('userprofile__role',)}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_role')
+
+    def get_role(self, obj):
+        return obj.userprofile.role
+    get_role.short_description = 'Role'
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        return super().get_inline_instances(request, obj)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
 admin.site.register(Book, BookAdmin)
+
