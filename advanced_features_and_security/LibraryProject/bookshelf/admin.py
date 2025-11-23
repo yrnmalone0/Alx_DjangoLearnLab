8a53dc862f1a18a4a1222cb14b8e6521aee34712
yrnmalone0 <obedbadu04@gmail.com>
2025-11-23 02:34:31 +0000

    Integrated the Custom User Model into Admin(bookshelf/admin.py)

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
 from ..relationship_app.models import UserProfile
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
@@ -38,8 +38,8 @@ class CustomUserAdmin(UserAdmin):
             return []
         return super().get_inline_instances(request, obj)
 
-admin.site.unregister(User)
-admin.site.register(User, CustomUserAdmin)
+admin.site.unregister(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
 
 admin.site.register(Book, BookAdmin)
 
