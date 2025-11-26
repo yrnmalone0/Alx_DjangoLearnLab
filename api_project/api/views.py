70f716c7c75161fbce323ded840f5d83fc11a785
yrnmalone0 <obedbadu04@gmail.com>
2025-11-26 18:04:47 +0000

    Defined permission classes

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -18,10 +18,27 @@ class BookViewSet(viewsets.ModelViewSet):
 
 # Implement a view that allows users to obtain a token by providing their username and password.
 # This can be done using DRF’s built-in views like obtain_auth_token.
-
-
 class CustomObtainAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
         token = Token.objects.get(key=response.data['token'])
         return Response({'token': token.key, 'user_id': token.user_id})
+
+
+"""Use rest_framework.permissions to apply basic permissions like IsAuthenticated, IsAdminUser, 
+or custom permissions based on your application’s needs. Modify your ViewSet configurations to 
+include the appropriate permissions."""
+from rest_framework import permissions
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Example permission
+
+# You can further customize permissions as needed for different actions
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            self.permission_classes = [permissions.AllowAny]
+        else:
+            self.permission_classes = [permissions.IsAuthenticated]
+        return super().get_permissions()
+
