e02216dca908c52ebb409a9d622104a04c3e85b6
yrnmalone0 <obedbadu04@gmail.com>
2025-11-26 18:08:30 +0000

    Defined permission classes

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -32,6 +32,17 @@ DEFAULT_AUTHENTICATION_CLASSES = [
     'rest_framework.authentication.TokenAuthentication',
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+}
+
+
 # Application definition
 
 INSTALLED_APPS = [
