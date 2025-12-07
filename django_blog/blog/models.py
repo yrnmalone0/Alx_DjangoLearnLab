1fe095d5078fbb4c4568c3f243e69c4d152c74f4
yrnmalone0 <obedbadu04@gmail.com>
2025-12-07 03:02:48 +0000

    Set Up Static and Template Directories

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,3 +9,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,7 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
