19e44d398eff9299e47d2da09416ab4a21d1c431
yrnmalone0 <obedbadu04@gmail.com>
2025-12-10 03:46:15 +0000

    fix: blog path modified

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
-    path('register', views.register, name='register'),
-    path('profile', views.profile, name='profile'),
+    path("login/", views.home, name="login"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
 ]
