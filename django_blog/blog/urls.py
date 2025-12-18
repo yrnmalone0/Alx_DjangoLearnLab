8d9165a5fff76c02c408821a88c7cdc78340780d
yrnmalone0 <obedbadu04@gmail.com>
2025-12-18 17:42:48 +0000

    feat(Comment): Added comment functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,8 +15,9 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="update-post"),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="delete-post"),
-    path('post/<int:post_id>/comments/new', CommentCreateView.as_view(), name="comment"),
+    #path('post/<int:post_id>/comments/new', CommentCreateView.as_view(), name="comment"),
+    path('post/<int:pk>/comments/new', CommentCreateView.as_view(), name="comment"),
     path('post/<int:post_id>/comments/', CommentListView.as_view(), name="comments"),
-    path('post/<int:pk>/comments/edit/', CommentUpdateView.as_view(), name="edit-comment"),
-    path('post/<int:pk>/comments/delete/', CommentDeleteView.as_view(), name="delete-comment"),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name="edit-comment"),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name="delete-comment"),
 ]
