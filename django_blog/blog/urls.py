831f0204292c70521d2501366615a5331dc24800
yrnmalone0 <obedbadu04@gmail.com>
2025-12-19 18:44:59 +0000

    implement tagging & searching functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name="edit-comment"),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name="delete-comment"),
     path('tag/<slug:tag_slug>/', views.posts_by_tag, name='posts-by-tag'),
-    path('search/', views.search_posts, name='search-posts')
+    path('search/', views.search_posts, name='search-posts'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view()),
 ]
